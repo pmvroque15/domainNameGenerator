6cb2afe13d15901ce2c3ecfb5d94ef5378a44a44
pmvroque15 <pmvroque@gmail.com>
2025-12-17 10:24:31 +0800

    Add loops and arrays"

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = [
+     'your', 'our', 'the', 'she', 'it', 'this'
+  ]
+  let adjective = ['great', 'big', 'massive', 'magnificent', 'lazy', 'lucky', 'cool', 'angry', 'favorite', 'horrible']
+  let noun = ['duck', 'cat', 'dog', 'grandma', 'circles', 'pc', 'tech', 'racoon', 'lion', 'tiger']
+  let topLevelDomain = ['.us', '.com', '.io','.edu','.ms']
+
+  for(let i of pronoun) {
+    for(let j of adjective) {
+      for(let k of noun) {
+        for(let l of topLevelDomain){
+        let randomPronoun = Math.floor(Math.random() * pronoun.length) 
+        let randomAdjective = Math.floor(Math.random() * adjective.length) 
+        let randomNoun = Math.floor(Math.random() * noun.length) 
+        let randomTopLevelDomain = Math.floor(Math.random() * topLevelDomain.length)
+        console.log(topLevelDomain[randomTopLevelDomain])
+        }
+      }
+    }
+  }
+
 };
